957d2308f1473d7820058bbfa22d8162e01ccfe8
AnnyP <annipan@seas.upenn.edu>
2022-10-03 16:24:46 -0400

    complete exercise

---
+++ 03/exercise.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    string str;
+    if (cin) {
+      num << cin;
+      str << cin;
+    }
+    cout << num << " " << str << endl;
+}
